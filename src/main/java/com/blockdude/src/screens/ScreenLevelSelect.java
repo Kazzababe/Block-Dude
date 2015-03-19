73d668b5e048f523f08fe78652cd9c64b035fd59
Mckelle Gravelle <RewindMods@gmail.com>
2015-03-19 14:52:38 -0500

    Replaced tile.

---
+++ src/main/java/com/blockdude/src/screens/ScreenLevelSelect.java
@@ -0,0 +1,24 @@
+package com.blockdude.src.screens;
+
+public class ScreenLevelSelect extends Screen {
+
+	@Override
+	public void update(float delta) {
+		
+	}
+
+	@Override
+	public void display(float delta) {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+	@Override
+	public void show() {
+		
+	}
+}
